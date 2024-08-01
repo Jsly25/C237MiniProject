a998dea0905e05c7e8ed62c26ed538890069862e
Jsly25 <23000291@myrp.edu.sg>
2024-08-01 10:13:13 +0800

    Database Credentials Updated

--- app.js
+++ app.js
@@ -9,11 +9,10 @@ const PORT = 3000;
 
 // Create MySQL connection
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'c237_workmanagementapp',
-    port: 3307
+    host: 'alwaysdata.com',
+    user: 'jerlene',
+    password: 'Jsly2005',
+    database: 'jerlene_mini_proj'
 });
 
 // Callback function to log whether the connection to the database was successful or not
